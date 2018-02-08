5209b1a029f0243e65399bedd9612563149217ea
Michsedki <michsedki12@gmail.com>
2018-02-07 20:03:34 -0800

    Added Files

---
+++ ScreenBoom/ScreenBoom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScreenBoom
+//
+//  Created by Michael Tanious on 2/7/18.
+//  Copyright © 2018 WMWiOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
